02f2e6ca324bf15bf0cd51cba1dc9e4db743aac4
qld2 <qld2@illinois.edu>
2018-05-21 14:10:58 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/HTML_Parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HTML_Parser.dir/main.cpp.o"
+  "HTML_Parser.pdb"
+  "HTML_Parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HTML_Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string findAttribute(string line) {
+    unsigned long separator = line.find("~");
+    return line.substr(separator);
+}
+
+string findTag(string line) {
+    unsigned long separator = line.find("~");
+    return line.substr(0, separator);
+}
+
+int findLineNumber(vector<string> lines, string input, int lineStart){
+    unsigned long firstPeriod = input.find(".");
+    unsigned long squiggly = input.find("~");
+    string name, otherTags;
+
+    if (firstPeriod == -1) {
+        name = input.substr(1, squiggly);
+    } else {
+        name = input.substr(1, firstPeriod);
+        otherTags = input.substr(firstPeriod,squiggly);
+    }
+
+    for (int i = lineStart; i < lines.size() / 2; i++) {
+        if (firstPeriod == -1) {
+            if (lines[i].find(name) != -1) {
+                return i;
+            }
+        } else {
+            if (lines[i].find(name) != -1) {
+                return findLineNumber(lines, otherTags, i);
+            }
+        }
+    }
+
+
+
+}
+
+
+
+string findAttributeValue(string line, string attribute) {
+    unsigned long startingIndex = line.find(attribute);
+    unsigned long quoteCounter = 0;
+    unsigned long start = 0, end = 0;
+
+    for (unsigned long i = startingIndex; i < line.length(); i++) {
+
+        if (line[i] == '\"') {
+            quoteCounter++;
+            if (quoteCounter == 1) {
+                start = i + 1;
+            }
+
+            if (quoteCounter == 2) {
+                end = i;
+                break;
+            }
+        }
+    }
+
+    return line.substr(start, end);
+}
+
+int main() {
+    unsigned long numOfLines, numOfQueries;
+    cin >> numOfLines >> numOfQueries;
+
+    vector<string> lines(numOfQueries);
+    vector<string> queries(numOfQueries);
+
+
+    for (int i = 0; i < numOfLines; i++) {
+        string in, temp;
+
+        cin >> temp;
+
+        while (temp != "\n") {
+            in += temp;
+        }
+
+        cout << in << endl;
+        lines[i] = in;
+    }
+    /*
+    for (unsigned long i = numOfLines; i < numOfLines + numOfQueries; i++) {
+        cin >> queries[i];
+    }
+
+
+    for (int i = 0; i < numOfQueries; i++) {
+        string attribute = findAttribute(queries[i]);
+        string tag = findTag(queries[i]);
+
+        int lineNum = findLineNumber(lines, tag, 0);
+
+        string attributeValue = findAttributeValue(queries[lineNum], attribute);
+        cout << attributeValue <<endl;
+    }
+
+    for (int i = 0; i < numOfQueries; i++) {
+        cout << queries[i] <<endl;
+    }
+    */
+
+    return 0;
+}
